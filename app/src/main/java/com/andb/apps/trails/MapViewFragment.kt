2d906b6f9ef8c5c9796cbfd33294a06950bfc705
andb3 <andb3003@yahoo.com>
2019-02-07 22:02:04 -0500

    - new pdf library, app size reduced to 10MB (debug size) from 32MB

--- app/src/main/java/com/andb/apps/trails/MapViewFragment.kt
+++ app/src/main/java/com/andb/apps/trails/MapViewFragment.kt
@@ -2,6 +2,7 @@ package com.andb.apps.trails
 
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -13,9 +14,11 @@ import com.andb.apps.trails.download.FileDownloader
 import com.andb.apps.trails.lists.FavoritesList
 import com.andb.apps.trails.objects.SkiMap
 import com.andb.apps.trails.xml.MapXMLParser
-import com.github.barteksc.pdfviewer.util.FitPolicy
+import com.davemorrissey.labs.subscaleview.ImageSource
 import com.nostra13.universalimageloader.core.ImageLoader
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener
+import de.number42.subsampling_pdf_decoder.PDFDecoder
+import de.number42.subsampling_pdf_decoder.PDFRegionDecoder
 import kotlinx.android.synthetic.main.map_view.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.android.Main
@@ -43,7 +46,6 @@ class MapViewFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         Log.d("MapViewFragment", "before thread")
-        mapPdfView.setBackgroundColor(Color.BLACK)
         mapLoadingIndicator.visibility = View.VISIBLE
         val handler = Handler()
         Thread(Runnable {
@@ -55,28 +57,27 @@ class MapViewFragment : Fragment() {
                     skiMapAreaName?.text = skiArea.name
                     skiMapYear?.text = year.toString()
                     if (!isPdf()) {
-                        skiMapImage?.apply {
-                            ImageLoader.getInstance().displayImage(
-                                imageUrl,
-                                this,
-                                object : SimpleImageLoadingListener() {
-                                    override fun onLoadingComplete(
-                                        imageUri: String?,
-                                        view: View?,
-                                        loadedImage: Bitmap?
-                                    ) {
-                                        mapLoadingIndicator.visibility = View.GONE
-                                        mapPdfView.visibility = View.GONE
+                        mapImageView?.apply {
+                            val image = ImageLoader.getInstance().loadImage(imageUrl, object : SimpleImageLoadingListener() {
+                                    override fun onLoadingComplete(imageUri: String?, view: View?, loadedImage: Bitmap?) {
+                                        if(loadedImage!=null) {
+                                            mapLoadingIndicator.visibility = View.GONE
+                                            mapImageView.setImage(ImageSource.bitmap(loadedImage))
+                                        }
                                     }
                                 })
-                            setColorFilter(Color.argb(0, 0, 0, 0))
+                            //setColorFilter(Color.argb(0, 0, 0, 0))
                         }
                     }else{
                         CoroutineScope(Dispatchers.IO).launch {
                             val file = FileDownloader.downloadFile(imageUrl, MapXMLParser.filenameFromURL(imageUrl))
                             withContext(Dispatchers.Main){
-                                mapPdfView.fromFile(file).pageFitPolicy(FitPolicy.BOTH).load()
-                                skiMapImage.visibility = View.GONE
+                                mapImageView.apply {
+                                    setMinimumTileDpi(120)
+                                    setBitmapDecoderFactory{PDFDecoder(0, file, 10f)}
+                                    setRegionDecoderFactory { PDFRegionDecoder(0, file, 10f) }
+                                    setImage(ImageSource.uri(file.absolutePath))
+                                }
                                 mapLoadingIndicator.visibility = View.GONE
                             }
                         }
